b658a90d8a4c687353b167d28dc6897c0f82d694
michaelraoof <raoofmichael@yahoo.com>
2024-08-21 21:55:56 +0300

    fix vercel

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,11 +1,11 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://dai3tna-ps1w51tg1-michaelraoofs-projects.vercel.app";
+    : "https://dai3tna.vercel.app";
 const baseUrlFE =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3001"
-    : "https://dai3tna-d6ur.vercel.app";
+    : "https://dai3tna-d6ur-awmieh5y1-michaelraoofs-projects.vercel.app";
 
 export default baseUrl;
 export { baseUrlFE };
